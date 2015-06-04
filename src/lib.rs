d1af6353234f939bce947b36683d05ac54e03dac
J/A <archer884@gmail.com>
2015-06-04 11:10:58 -0400

    Move dice to own module

--- src/lib.rs
+++ src/lib.rs
@@ -3,50 +3,11 @@
 extern crate rand;
 extern crate regex;
 
+mod dice;
 mod error;
 mod result;
 mod tests;
 
-use regex::Regex;
-
+pub use dice::Dice;
 pub use error::DiceParseError;
 pub use result::{ DiceResult, DiceResultGenerator };
-
-static DICE_CMD_PATTERN: Regex = regex!(r"^\d+(d\d+)?$");
-
-/// Describes a set of dice of the same type that can be "rolled" all at once, i.e. "2d6"
-#[derive(Debug, Eq, PartialEq)]
-pub struct Dice {
-    pub count: u32,
-    pub range: u32,
-}
-
-impl Dice {
-    pub fn new(count: u32, range: u32) -> Dice {
-        Dice {
-            count: count,
-            range: range,
-        }
-    }
-
-    pub fn gen_result<G: DiceResultGenerator>(&self, drg: &mut G) -> DiceResult {
-        drg.gen_result(&self)
-    }
-}
-
-impl ::std::str::FromStr for Dice {
-    type Err = DiceParseError;
-
-    fn from_str(s: &str) -> Result<Dice, Self::Err> {
-        if !DICE_CMD_PATTERN.is_match(s) {
-            return Err(DiceParseError::InvalidExpression)
-        }
-
-        let split: Vec<_> = s.split('d').filter_map(|n| n.parse().ok()).collect();
-        match &split[..] {
-            [ref count, ref range] => Ok(Dice::new(*count, *range)),
-            [ref range] => Ok(Dice::new(1, *range)),
-            _ => Err(DiceParseError::InvalidExpression),
-        }
-    }
-}
